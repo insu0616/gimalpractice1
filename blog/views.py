bd349737cecd5f672b53098e5a87ff38f7bd3be6
insoo <inu0616@gmail.com>
2016-06-04 13:05:53 +0900

    detail페이지 구현

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post, Comment
 # Create your views here.
 
 def index(request):
     post_list = Post.objects.all()
     return render(request, 'blog/index.html', {'post_list': post_list})
+
+def post_detail(request, pk):
+    post_detail = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/detail.html', {'post_detail': post_detail})
